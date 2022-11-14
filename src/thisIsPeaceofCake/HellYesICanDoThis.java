3fc5bcebbcb72f6b684a48b214f2d76782e20271
BenzadShafiei <behzad53sh@gmail.com>
2022-11-13 22:22:58 -0500

    this is on practice branch

--- src/thisIsPeaceofCake/HellYesICanDoThis.java
+++ src/thisIsPeaceofCake/HellYesICanDoThis.java
@@ -1,5 +1,11 @@
 package thisIsPeaceofCake;
 
 public class HellYesICanDoThis {
+	public void iCanLearnGit() {
+		System.out.println("I learn this or i will fast for rest of the night");
+	}
+	public void thisIsFunWhenItWorks() {
+		System.out.println("i will learn it regardless of issues i am facing");
+	}
 
 }
